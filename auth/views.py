5409a6a154a6dd29921e7f5781b72ff9f2d8eb99
Lubega Mark <lubegamark@gmail.com>
2017-06-10 17:12:53 +0300

    Correct Permission for User Registration

--- auth/views.py
+++ auth/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 from auth.serializers import UserSerializer
 
 
 class UserRegistrationView(generics.ListCreateAPIView):
-    permission_classes = ['AllowAny',]
+    permission_classes = [AllowAny, ]
     serializer_class = UserSerializer
     queryset = get_user_model().objects
