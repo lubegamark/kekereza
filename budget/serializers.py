d8f12e868e41a5348f4a62c74d51865d2af6e0e8
Lubega Mark <lubegamark@gmail.com>
2017-06-10 12:08:58 +0300

    Add serializer fields

--- budget/serializers.py
+++ budget/serializers.py
@@ -6,8 +6,10 @@ from budget.models import Income, Expense
 class IncomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Income
+        fields = '__all__'
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
+        fields = '__all__'
