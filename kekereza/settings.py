f74f75ceddc63b9817473d3160399f50fc19a384
Lubega Mark <lubegamark@gmail.com>
2017-06-10 13:09:29 +0300

    Add auth app to project

--- kekereza/settings.py
+++ kekereza/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'budget.apps.BudgetConfig',
     'rest_framework',
+    'budget.apps.BudgetConfig',
+    'auth.apps.AuthConfig',
 ]
 
 MIDDLEWARE = [
--- kekereza/urls.py
+++ kekereza/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^budget/', include('budget.urls')),
+    url(r'^users/', include('auth.urls'))
 ]
